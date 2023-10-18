3f47d4eff78c22232021189d9b08d37ebbe596c9
Bhadriraju Venkata Naga Sameera Diwakar <diwakar1016@gmail.com>
2023-10-18 14:26:44 +0530

    Update urlRoutes.js

--- routes/urlRoutes.js
+++ routes/urlRoutes.js
@@ -4,11 +4,10 @@ const router = express.Router();
 
 // URL Shortening route
 router.post('/shorten', async (req, res) => {
-  // Handle URL shortening logic here
 });
 
 // URL Redirection route
 router.get('/:shortCode', async (req, res) => {
-  // Handle URL redirection logic here
 });
+
 module.exports = router;
