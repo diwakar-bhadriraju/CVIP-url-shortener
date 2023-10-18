c40aae37c25b6dd3efee91e69209584910a857a8
Bhadriraju Venkata Naga Sameera Diwakar <diwakar1016@gmail.com>
2023-10-18 14:26:56 +0530

    Update URLModel.js

--- models/URLModel.js
+++ models/URLModel.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const urlSchema = new mongoose.Schema({
   longURL: String,
   shortURL: String,
-  shortCode: String, // Make sure the field matches your model
+  shortCode: String,
 });
 const URL = mongoose.model('URL', urlSchema, 'urls');
 
